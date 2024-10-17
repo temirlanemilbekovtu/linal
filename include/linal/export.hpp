d7577486286995e404401af2dffe4f3cf3015fef
Temirlan Emilbekov <emilbekov.tute@gmail.com>
2024-10-17 22:32:52 +0600

    Refactor export.hpp

--- include/linal/export.hpp
+++ include/linal/export.hpp
@@ -3,11 +3,17 @@
 #ifndef LINAL_EXPORT_HPP
 #define LINAL_EXPORT_HPP
 
-#if defined(_WIN32) || defined(_WIN64)
-    #if defined(LINAL_EXPORTS)
+#if defined(_WIN32) || defined(__CYGWIN__)
+    #ifdef LINAL_EXPORTS
         #define LINAL_API __declspec(dllexport)
     #else
         #define LINAL_API __declspec(dllimport)
+    #endif
+#elif defined(__GNUC__) && __GNUC__ >= 4
+    #ifdef LINAL_EXPORTS
+        #define LINAL_API __attribute__((visibility("default")))
+    #else
+        #define LINAL_API
     #endif
 #else
     #define LINAL_API
