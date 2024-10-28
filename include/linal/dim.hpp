dc58fcfadd65ce3516afaba0ac14fa1b3a2aff10
Temirlan Emilbekov <emilbekov.tute@gmail.com>
2024-10-28 23:48:35 +0600

    Update Dim struct
    - Incapsulated fields.
    - Add cardinality as `card` field.

--- include/linal/dim.hpp
+++ include/linal/dim.hpp
@@ -8,15 +8,24 @@
 #include "export.hpp"
 
 struct LINAL_API Dim {
+private:
     size_t rows;
     size_t cols;
+    size_t card;
 
+    void update_card();
+
+public:
     explicit Dim(const size_t rows, const size_t cols);
 
     Dim &operator=(const Dim &other);
     bool operator==(const Dim &other) const;
     bool operator!=(const Dim &other) const;
 
+    size_t get_rows() const;
+    void   set_rows(const size_t value);
+    size_t get_cols() const;
+    void   set_cols(const size_t value);
     size_t get_card() const;
 };
 
--- src/dim.cpp
+++ src/dim.cpp
@@ -2,11 +2,8 @@
 
 Dim::Dim(const size_t rows, const size_t cols)
     : rows(rows)
-    , cols(cols) { }
-
-size_t Dim::get_card() const {
-    return rows * cols;
-}
+    , cols(cols)
+    , card(rows * cols) { }
 
 Dim &Dim::operator=(const Dim &other) {
     if (this != &other) {
@@ -23,4 +20,30 @@ bool Dim::operator==(const Dim &other) const {
 
 bool Dim::operator!=(const Dim &other) const {
     return rows != other.rows || cols != other.cols;
+}
+
+void Dim::update_card() {
+    card = rows * cols;
+}
+
+size_t Dim::get_rows() const {
+    return rows;
+}
+
+void Dim::set_rows(const size_t value) {
+    rows = value;
+    update_card();
+}
+
+size_t Dim::get_cols() const {
+    return cols;
+}
+
+void Dim::set_cols(const size_t value) {
+    cols = value;
+    update_card();
+}
+
+size_t Dim::get_card() const {
+    return card;
 }
