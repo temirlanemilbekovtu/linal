815803be5f3e303f1894be30285afb1a191a285a
Temirlan Emilbekov <emilbekov.tute@gmail.com>
2024-10-31 21:18:40 +0600

    Update Utils: refactor `is_power_of_two` function
    - Removed unnecessary non-negative check from the function.

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,7 +1,7 @@
 #include "linal/utils.hpp"
 
 bool Utils::is_power_of_two(const uint32_t n)  {
-    return n > 0 && (n & (n - 1)) == 0;
+    return (n & (n - 1)) == 0;
 }
 
 uint32_t Utils::bit_ceil(uint32_t n) {
