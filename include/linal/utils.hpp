472b817870da43519ca8f97178dc916eb2b489f4
Temirlan Emilbekov <emilbekov.tute@gmail.com>
2024-10-31 20:48:43 +0600

    Update Util
    - Changed `int` to `uint32_t` for `bit_ceil` and `is_power_of_two` functions' parameters and return types to ensure compatibility with fixed-width unsigned types.

--- include/linal/utils.hpp
+++ include/linal/utils.hpp
@@ -3,10 +3,12 @@
 #ifndef LINAL_UTILS_HPP
 #define LINAL_UTILS_HPP
 
+#include "cstdint"
+
 class Utils {
 public:
-    static int bit_ceil(const int n);
-    static bool is_power_of_two(const int n);
+    static uint32_t bit_ceil(const uint32_t n);
+    static bool is_power_of_two(const uint32_t n);
 };
 
 #endif //LINAL_UTILS_HPP
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,10 +1,10 @@
 #include "linal/utils.hpp"
 
-bool Utils::is_power_of_two(const int n)  {
+bool Utils::is_power_of_two(const uint32_t n)  {
     return n > 0 && (n & (n - 1)) == 0;
 }
 
-int Utils::bit_ceil(const int n) {
+uint32_t Utils::bit_ceil(const uint32_t n) {
     int i = sizeof(n) * 8;
     bool bit;
     do {
